bb6a9e7c58e4b3f4bcaf5d33a3b20040b7792f49
Isaiah Kang <i.say.yaa@gmail.com>
2022-03-17 16:06:07 -0400

    cb function

--- apartments/index.js
+++ apartments/index.js
@@ -5,14 +5,6 @@ let apt1 = {
     windows: 0,
     rent: 4500,
     unit: "1A",
-    lease: function(tenant) {
-        if (this.tenants.length ===this.bedrooms){
-            return `${this.unit} is already full.`
-        }
-        this.tenants.push(tenant)
-        console.log(tenant.name, 'has rented out', this.unit)
-
-    },
     tenants: []
 }
 
@@ -66,6 +58,14 @@ let building = {
     streetAddress:'11 Braodway',
     laundry: false,
     allowsPets: false,
+    lease: function(apt, tenant) {
+        if (apt.tenants.length ===this.bedrooms){
+            return `${apt.unit} is already full.`
+        }
+        apt.tenants.push(tenant)
+        console.log(tenant.name, 'has rented out', apt.unit)
+
+    },
     apartments: [apt1, apt2, apt3]
 }
 
