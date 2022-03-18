672bcfd498e68f12017dcdb0ecb94a920b68797a
Isaiah Kang <i.say.yaa@gmail.com>
2022-03-18 11:40:40 -0400

    more object practice

---
+++ apartments/example_objects.js
@@ -0,0 +1,58 @@
+const laptop = {
+    price: 1000,
+    brand: 'HP',
+    memory: '2GB',
+    color: 'pink'
+}
+
+const car = {
+    engine: 'V8',
+    doors: 2,
+    model: 'BMW',
+    price: 200000,
+    make: 'M4'
+}
+
+const company = {
+    name: 'Vittles',
+    ceo: 'Michael Law',
+    isTheBest: true,
+    publiclyTraded: false,
+}
+
+const room = {
+    windows: 0,
+    dimensions: '200x200',
+    closet: false,
+    isLegal: false,
+    pets: false
+}
+
+const item = {
+    name: 'Excalibur',
+    attackPower: 130,
+}
+
+const ally = {
+    name: 'Geralt',
+    power: { name: 'Kamehameha', attackPower: 9000}
+}
+
+
+const character = {
+   hairColor: 'blue',
+   height: "6'7",
+   race: 'Elf' ,
+   gender: null,
+   strength: Infinity,
+   dlc: false,
+   class: 'Mage',
+   cohort: '031422',
+   item: item,
+   ally: ally
+}
+
+character.ally.power['manaCost'] = 50
+
+
+console.log(character, laptop, room, company)
